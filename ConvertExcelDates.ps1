54020fa5cd69be5f0cb1ded269553115b76eaae1
Mateusz-Malendowski <81644484+Mateusz-Malendowski@users.noreply.github.com>
2024-02-05 10:05:31 +0100

    fix

--- ConvertExcelDates.ps1
+++ ConvertExcelDates.ps1
@@ -7,13 +7,16 @@ Get-ChildItem -Filter *.xlsx | ForEach-Object  {
     $file = Import-Excel -Path $_.Name
     $list = $file[0].PSObject.Properties | ForEach-Object {$_.Name}
     $list = [System.Collections.Generic.List[String]]$list
+    $tempList = [System.Collections.Generic.List[String]]::new()
+    $list | ForEach-Object {$tempList.Add($_)}
+    foreach($column in $list)
+    {
+        try { [datetime]$file[0].($column) | Out-Null }
+        catch { $tempList.Remove($column) | Out-Null } 
+    }
+    $list = $tempList
     for($i=0; $i -lt $file.Count; $i++)
     {
-        if ($i -eq 0)
-        {
-            $tempList = [System.Collections.Generic.List[String]]::new()
-            $list | ForEach-Object {$tempList.Add($_)}
-        }
         foreach($column in $list)
         {
             try
@@ -21,12 +24,10 @@ Get-ChildItem -Filter *.xlsx | ForEach-Object  {
                 if(([datetime]$file[$i].($column)) -eq ([datetime]"01.01.1900")){$file[$i].($column)=([datetime]$file[$i].($column)).AddDays(-1)}
                 else{$file[$i].($column)=([datetime]$file[$i].($column))}
             }
-            catch { if ($i -eq 0){$tempList.Remove($column) | Out-Null } }
+            catch {}
         }
-        if ($i -eq 0) {$list = $tempList}
     }
-    
     $worksheet = Get-ExcelSheetInfo $_.Name
-    $file | Export-Excel -Path $path -AutoFilter -NoNumberConversion * -WorksheetName $worksheet[0].Name
+    $file | Export-Excel -Path $_.Name -AutoFilter -NoNumberConversion * -WorksheetName $worksheet[0].Name
     Write-Host $_.Name "processed successfully" -ForegroundColor Green
 }
